f0797c89a96718030b6a5a2bdf1a1aa746c3adaa
John Smoker <vinniethepoolo@protonmail.com>
2020-09-11 00:52:04 +0500

    cleaned up sandbox css playing around with css variables in template

---
+++ utils.js
@@ -0,0 +1,18 @@
+/**
+ * Returns a degree value as radian for the purpose of degree calculations of &lt;canvas&gt; element
+ * @param {number} degrees 
+ */
+export function degToRad(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+/**
+ * Rolls random value between `min` and `max` and returns the result;
+ * 
+ * TODO: provide type checking and conversions
+ * @param {number} min
+ * @param {number} max
+ */
+function rollRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + (min);
+}
